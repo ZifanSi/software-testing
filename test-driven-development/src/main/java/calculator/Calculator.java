c8867d8f623ace5e2d579bcfadcc1cb3347c1e19
Zifan Si <siz@mcmaster.ca>
2025-02-06 22:58:45 -0500

    TDD Iteration 2: Implement divide() method to pass tests

---
+++ test-driven-development/src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+/**
+ * Simple Calculator class.
+ */
+public class Calculator {
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
